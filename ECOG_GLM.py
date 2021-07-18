855e7fe2125cbdd6aac24cfc5387599c816247fc
JinhuaTian <38090292+JinhuaTian@users.noreply.github.com>
2021-07-18 22:44:01 +0800

    ｆｉｘ　ＧＬＭ

--- ECOG_GLM.py
+++ ECOG_GLM.py
@@ -60,18 +60,29 @@ for i in range(windowLength,dat1['stim_id'].shape[0]):
     newLabel[index] = tmpLabel[::-1] # invert the label number
     index = index + 1
 
+# calculate model RDM
 for i in range(newLabel.shape[0]):
     for n in range(newLabel.shape[1]):
         labelNum[i] = labelNum[i] + (2**n)*newLabel[i,n]
     if newLabel[i,0] != newLabel[i,1] and newLabel[i,1] == newLabel[i,2] and newLabel[i,2] == newLabel[i,3]:
         novelLevel[i] = 3
+        adaptLevel[i] = 0
     elif newLabel[i,0] != newLabel[i,1] and newLabel[i,1] == newLabel[i,2] and newLabel[i,2] != newLabel[i,3]:
         novelLevel[i] = 2
+        adaptLevel[i] = 0
     elif newLabel[i,0] != newLabel[i,1] and newLabel[i,1] != newLabel[i,2]:
         novelLevel[i] = 1
-    elif newLabel[i,0] == newLabel[i,1]:
+        adaptLevel[i] = 0
+    elif newLabel[i,0] == newLabel[i,1] and newLabel[i,1] != newLabel[i,2] :
         novelLevel[i] = 0
-
+        adaptLevel[i] = 1
+    elif newLabel[i,0] == newLabel[i,1] and newLabel[i,1] == newLabel[i,2] and newLabel[i,2] != newLabel[i,3]:
+        adaptLevel[i] = 2
+        novelLevel[i] = 0
+    elif newLabel[i,0] == newLabel[i,1] and newLabel[i,1] == newLabel[i,2] and newLabel[i,2] == newLabel[i,3]:
+        adaptLevel[i] = 3
+        novelLevel[i] = 0
+        
 # compute pair number:
 pairNum = 0
 for x in range(labelNum[0]):
@@ -90,9 +101,12 @@ for t in range(80):
     avgV_epochs[:,t,:] = np.average(V_epochs[:,t*10:t*10+10,:],axis=1)
 nSample, nTime, nChan = avgV_epochs.shape
 avgV_epochs = avgV_epochs.reshape(nSample*nTime, nChan)
+# normalize data
+scaler = StandardScaler()
+avgV_epochs = scaler.fit_transform(avgV_epochs)
 avgV_epochs = avgV_epochs.reshape(nSample,nTime, nChan)
 
-rMatrix = np.zeros([len(picks),nTime,2])
+rMatrix = np.zeros([len(picks),nTime,3])
 # run GLM or linear regression
 # coef: newLabel[0,:], novelLevel
 method = 'LR'
@@ -101,13 +115,13 @@ for pick in picks:
     pickEpochs = avgV_epochs[:,:,pick]
     for tps in range(nTime):
         pickTps = pickEpochs[:,tps,]
-        x = np.concatenate((newLabel[:,0].reshape(297,1),novelLevel.reshape(297,1)),axis = 1)
+        x = np.concatenate((newLabel[:,0].reshape(297,1),novelLevel.reshape(297,1),adaptLevel.reshape(297,1)),axis = 1)
         if method == 'LR':
             linreg = LR()
             model = linreg.fit(x,pickTps)
             rMatrix[pickNum,tps,:] = linreg.coef_
         elif method == 'GLM':
-            glmModel = sm.GLM(x,pickTps)
+            glmModel = sm.GLM(pickTps,x)
             glm_results = glmModel.fit() # glm_results.summary()
             param = glm_results.params
             rMatrix[pickNum,tps,:] = param
@@ -117,8 +131,8 @@ for pick in range(len(picks)):
     chanName = ' Channel ' + str(picks[pick])
     fig = plt.figure()
     plt.plot(np.arange(80),rMatrix[pick,:,0], label='Stimuli type', color='brown')
-    plt.plot(np.arange(80),rMatrix[pick,:,1], label='Adaptation level', color='mediumblue')
-    
+    plt.plot(np.arange(80),rMatrix[pick,:,1], label='Novel level', color='mediumblue')
+    plt.plot(np.arange(80),rMatrix[pick,:,2], label='Adapt level', color='forestgreen')
     plt.xlabel('Time points(10ms)')
     plt.ylabel('Coefficient weights')
     plt.title('Time course of regression coefficient' + chanName)
